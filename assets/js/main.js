7fce79110f405d022f598feb24c4dd493325ca1a
Apurv Chandel <apurvchandel19@gmail.com>
2020-01-22 01:15:18 +0530

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
     });
 
     $(".countdown")
-        .countdown("2020/02/22", function(event) {
+        .countdown("2020/02/23", function(event) {
             $(this).html(
                 event.strftime(' <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
